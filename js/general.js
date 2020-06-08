8803788abf6231020161d5309c02f856c7e29b2a
Will <khaitran610@gmail.com>
2020-06-08 15:40:13 +0700

    will
    done layout PC

--- js/general.js
+++ js/general.js
@@ -1,5 +1,7 @@
 $(function(){
-
+	$(window).on('load scroll',function() {
+		scrollToElement('.listTimeLine__linePoint', 'draw');
+	});
 	function scrollToElement(classFirst, nameClassAdd) {
 		$(classFirst).each(function() {
 			var spaceTop = $(this).offset().top;
@@ -7,8 +9,17 @@ $(function(){
 			var spaceBottom = $(document).height() - $(this).offset().top - chieucao;
 			var scrollBottom = $(document).height() + $(window).height() - $(window).scrollTop();
 			var scrollSpaceTop = $(window).scrollTop();
-			if ((spaceBottom < scrollBottom - $(window).height()) && (spaceTop + (chieucao/2) < scrollSpaceTop + $(window).height())) {
-				$(this).addClass(nameClassAdd);
+			if ((spaceBottom < scrollBottom - $(window).height()) && (spaceTop + (chieucao / 2) < scrollSpaceTop + $(window).height())) {
+				
+				$(this).children(0).addClass(nameClassAdd);
+				$(this).children(1).addClass(nameClassAdd);
+
+				if($(this).children(1).hasClass(nameClassAdd)){
+					setTimeout(function() {
+						$(this).children(2).addClass(nameClassAdd);
+					}, 1000);
+					$(this).next().children(0).addClass(nameClassAdd);
+				}
 			}
 		});
 	}
